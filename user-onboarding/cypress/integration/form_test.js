05575a9706205e18e9d2b32caa17ae050e0b537d
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-10-14 13:41:55 -0700

    input tests working

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -8,16 +8,32 @@ describe('Add Account Form', () => {
 
     // Getters
     const firstNameInput = () => cy.get('input[name=first_name]');
+    const emailInput = () => cy.get('input[name=email]');
+    const passwordInput= () => cy.get('input[name=password]');
+
 
     describe('inputs working', () => {
 
         it('can enter text in first name', () => {
             firstNameInput()
-            .should('have.value', '')
-            .type('first name')
-            .should('have.value', 'first name')
+                .should('have.value', '')
+                .type('first name')
+                .should('have.value', 'first name')
         })
 
+        it('can enter text in email input', () => {
+            emailInput()
+                .should('have.value', '')
+                .type('email@aol.com')
+                .should('have.value', 'email@aol.com')
+        })
+
+        it('can enter text in password input', () => {
+            passwordInput()
+                .should('have.value', '')
+                .type('testpassword')
+                .should('have.value', 'testpassword')
+        })
     })
 
 
